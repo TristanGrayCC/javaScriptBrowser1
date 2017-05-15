29dba5355495f7f5b27423ce10c9683c70882322
Tristan Gray <tristangraycc@gmail.com>
2017-05-15 17:04:23 +0100

    completed first extension

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -10,6 +10,15 @@ createList = function(info){
   return item;
 };
 
+createImage = function(img){
+  var item = document.createElement("li");
+  var image = document.createElement("img");
+  image.setAttribute("src", img);
+  image.setAttribute("height", "150px");
+  item.appendChild(image);
+  return item;
+};
+
 appendElements = function(section, name, food, image){
   section.appendChild(name);
   section.appendChild(food);
@@ -19,14 +28,17 @@ appendElements = function(section, name, food, image){
 };
 
 var CAT_ARRAY = [
-  { name: "Rollo", favourite_food: "Anything", image: "https://scontent.xx.fbcdn.net/v/t31.0-8/17917916_10213301392038148_7901082137114936539_o.jpg?oh=8f8af800c6f0ed6740e580abda77b8f0&oe=59C0A7F9" }
+  { name: "Name: Rollo", favourite_food: "Favourite food: Anything", image:"https://scontent.xx.fbcdn.net/v/t31.0-8/17917916_10213301392038148_7901082137114936539_o.jpg?oh=8f8af800c6f0ed6740e580abda77b8f0&oe=59C0A7F9" },
+  { name: "Name: Boba", favourite_food: "Favourite food: Sock fluff", image:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg" },
+  { name: "Name: Barnaby", favourite_food: "Favourite food: Tuna", image: "http://dreamicus.com/data/cat/cat-01.jpg" },
+  { name: "Name: Max", favourite_food: "Favourite food: Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg" }
 ];
 
 var addCat = function(name, favourite_food, image) {
   var catSection = createCatSection();
   var name = createList(name);
   var food = createList(favourite_food);
-  var image = createList(image);
+  var image = createImage(image);
 
   appendElements(catSection, name, food, image);
 }
