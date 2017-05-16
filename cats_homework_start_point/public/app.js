317628641d8ac72f5ec32d2191d4e95669af29ef
Tristan Gray <tristangraycc@gmail.com>
2017-05-16 09:23:16 +0100

    tidied up design

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -4,8 +4,15 @@ createCatSection = function(){
   return newListItem;
 };
 
-createList = function(info){
+createFood = function(info){
   var item = document.createElement("li");
+  item.innerText = "Favourite food: " + info;
+  return item;
+};
+
+createName = function(info){
+  var item = document.createElement("li");
+  item.classList.add("name");
   item.innerText = info;
   return item;
 };
@@ -28,16 +35,16 @@ appendElements = function(section, name, food, image){
 };
 
 var CAT_ARRAY = [
-  { name: "Name: Rollo", favourite_food: "Favourite food: Anything", image:"https://scontent.xx.fbcdn.net/v/t31.0-8/17917916_10213301392038148_7901082137114936539_o.jpg?oh=8f8af800c6f0ed6740e580abda77b8f0&oe=59C0A7F9" },
-  { name: "Name: Boba", favourite_food: "Favourite food: Sock fluff", image:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg" },
-  { name: "Name: Barnaby", favourite_food: "Favourite food: Tuna", image: "http://dreamicus.com/data/cat/cat-01.jpg" },
-  { name: "Name: Max", favourite_food: "Favourite food: Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg" }
+  { name: "Rollo", favourite_food: "Anything", image:"https://scontent.xx.fbcdn.net/v/t31.0-8/17917916_10213301392038148_7901082137114936539_o.jpg?oh=8f8af800c6f0ed6740e580abda77b8f0&oe=59C0A7F9" },
+  { name: "Boba", favourite_food: "Sock fluff", image:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg" },
+  { name: "Barnaby", favourite_food: "Tuna", image: "http://dreamicus.com/data/cat/cat-01.jpg" },
+  { name: "Max", favourite_food: "Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg" }
 ];
 
 var addCat = function(name, favourite_food, image) {
   var catSection = createCatSection();
-  var name = createList(name);
-  var food = createList(favourite_food);
+  var name = createName(name);
+  var food = createFood(favourite_food);
   var image = createImage(image);
 
   appendElements(catSection, name, food, image);
